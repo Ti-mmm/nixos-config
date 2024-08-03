da3fc95ca8d0518d8ed265223ee3d72ca0a5561b
Ti-mmm <weissflog.tim@icloud.com>
2024-08-03 13:44:33 +0200

    change hyprland.nix for more compatibility

--- nixosModules/hyprland.nix
+++ nixosModules/hyprland.nix
@@ -13,10 +13,10 @@
   # fix cursor flickering:
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
-  # Install required Software
+  # Install related Software
   environment.systemPackages = with pkgs; [
     qt5ct
-    hyprpaper
+    hyprpaper # Wallpapers
     hypridle
     hyprlock
     hyprcursor
@@ -27,11 +27,28 @@
     libnotify
     rofi-wayland
     nvidia-vaapi-driver
+
+    # might fix some issues
+    wayland-protocols
+    wayland-utils
+    meson
+
+    # Desktop portals
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland
+    xwayland
   ];
+
   security.polkit.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals =
-    [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
+
+  # Enable Sceensharing
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals =
+      [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
+  };
 
   #services.displayManager.sddm = {
   #enable = true;
